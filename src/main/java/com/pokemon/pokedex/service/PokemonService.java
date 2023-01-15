0eece47bc5ddb8a09edd0309ef056ff0c6e951b4
Omar Domínguez Fuentes <omardominguezfuentes@gmail.com>
2023-01-15 17:13:54 +0000

    Pokemon Controller Terminado
    Implementación de búsqueda de pokemons por nombre, por número de pokedex, búsqueda de todos los pokemon y búsqueda de pokemon filtrando por tipo.

--- src/main/java/com/pokemon/pokedex/service/PokemonService.java
+++ src/main/java/com/pokemon/pokedex/service/PokemonService.java
@@ -1,9 +1,18 @@
 package com.pokemon.pokedex.service;
 
+import java.util.List;
+
 import com.pokemon.pokedex.entity.Pokemon;
+import com.pokemon.pokedex.entity.Type;
 
 public interface PokemonService {
 	
 	public Pokemon findByName(String name);
+	
+	public List<Pokemon> findAll();
+	
+	public List<Pokemon> findAllByType(Type type);
+	
+	public Pokemon findByNumPokedex(int numPokedex);
 
 }
