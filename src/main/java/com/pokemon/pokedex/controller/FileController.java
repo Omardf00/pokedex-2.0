1fe9fba7ad7d2f65a826fa290196e27b63c8e730
Omar Domínguez Fuentes <omardominguezfuentes@gmail.com>
2023-03-01 21:15:56 +0000

    Finalización de subida y listado de archivos

--- src/main/java/com/pokemon/pokedex/controller/FileController.java
+++ src/main/java/com/pokemon/pokedex/controller/FileController.java
@@ -1,19 +1,25 @@
 package com.pokemon.pokedex.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import es.pokemon.pokedex.utils.FileUtils;
+import es.pokemon.pokedex.utils.FileResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
@@ -33,18 +39,49 @@ public class FileController {
 		
 		try {
 			
-			byte[] bytesFromFile = new byte[0];
-			bytesFromFile = file.getBytes();
+			String fileName = file.getOriginalFilename();
+			Path path = Paths.get("./src/main/resources/media").resolve(fileName).toAbsolutePath();
 			
-			Files.write(Paths.get(FileUtils.folderPath + file.getOriginalFilename()), bytesFromFile);
+			Files.copy(file.getInputStream(), path);
 			
-		} catch (Exception e) {
-			response.put("message", "There was an error uploading the file");
+		} catch (IOException e) {
+			response.put("message", "There was an error uploading the file. Try changing the name");
 			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		response.put("message", "The file " + file.getName() + " has been successfully uploaded");
-		return new ResponseEntity<>(response, HttpStatus.OK);
+		response.put("message", "The file " + file.getOriginalFilename() + " has been successfully uploaded");
+		return new ResponseEntity<>(response, HttpStatus.CREATED);
+		
+	}
+	
+	@GetMapping("/")
+	public ResponseEntity<?> listFiles(){
+		
+		FileResponse fileResponse = new FileResponse();
+		List<String> filesList = new ArrayList<>();
+		Map<String, Object> response = new HashMap<>();
+		
+		try {
+			
+			String directoryPath ="./src/main/resources/media";
+			File directory = new File(directoryPath);
+			
+			File[] files = directory.listFiles();
+			
+			for(File file : files) {
+				if (file.isFile()) {
+					filesList.add(file.getName());
+				}
+			}
+			
+			fileResponse.setNames(filesList);
+			
+		} catch (Exception e) {
+			response.put("message", "The service is not available");
+			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		return new ResponseEntity<>(fileResponse, HttpStatus.OK);
 		
 	}
 }
---
+++ src/main/java/es/pokemon/pokedex/utils/FileResponse.java
@@ -0,0 +1,12 @@
+package es.pokemon.pokedex.utils;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class FileResponse {
+	
+	private List<String> names;
+
+}
